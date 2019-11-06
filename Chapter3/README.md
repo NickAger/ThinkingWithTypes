2bfe423bc212d960ed56fb102cf4f7e67098d0ba
Nick Ager <nick.ager@gmail.com>
2019-11-06 09:48:58 +0000

    Chapter 3 WIP

---
+++ Chapter3/README.md
@@ -0,0 +1,4 @@
+# Chapter3: Variance
+
+See also:
+* [George Wilson - Extended Functor family](https://www.youtube.com/watch?v=JZPXzJ5tp9w)
---
+++ Chapter3/src/Main.hs
@@ -0,0 +1,49 @@
+{-# LANGUAGE InstanceSigs #-}
+module Main where
+
+import           GHC.TypeLits
+
+{-
+Consider the following type declarations. Which have __viable__ `Functor` instances:
+-}
+
+newtype T1 a = T1 (Int -> a)
+
+newtype T2 a = T2 (a -> Int)
+
+newtype T3 a = T3 (a -> a)
+
+newtype T4 a = T4 ((Int -> a) -> Int)
+
+newtype T5 a = T5 ((a -> Int) -> Int)
+
+-- see: [George Wilson - Extended Functor family](https://www.youtube.com/watch?v=JZPXzJ5tp9w)
+
+testT1 :: T1 String -> Int -> String
+testT1 (T1 f) = f
+
+instance Functor T1 where
+  fmap :: (a -> b) -> T1 a -> T1 b
+  fmap f (T1 f2) = T1 $ f . f2
+
+-- not possible to create T2
+-- can't create (b -> Int) from (a -> b) and (a -> Int)
+-- instance Functor T2 where
+--   fmap f (T2 f2) = T2 $ f2 . f
+
+-- instance Functor T3 where
+--   fmap f (T3 f2) = T3 $ f . f2
+
+{-
+data FixMePls a =
+    FixMe
+  | Pls a
+  deriving (Eq, Show)
+  
+instance Functor FixMePls where
+  fmap _ FixMe = FixMe
+  fmap f (Pls a) = Pls (f a)
+
+-}
+
+main = putStrLn "Chapter3"
